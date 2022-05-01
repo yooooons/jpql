8c016415e8eca85d24994529afe77f21a6e4d3fc
unknown <yooooonnf@gmail.com>
2022-05-01 23:07:43 +0900

    객체지향 쿼리 언어1 - 기본 문법

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -19,10 +19,17 @@ public class JpaMain {
             member.setUsername("member1");
             member.setAge(10);
             em.persist(member);
-            TypedQuery<Member> query = em.createQuery("select  m from Member  m where m.username = :username", Member.class);
-            query.setParameter("username", "member1");
-            Member singleResult = query.getSingleResult();
-            System.out.println("singleResult = " + singleResult.getUsername());
+
+            em.flush();
+            em.clear();
+
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+
 
             tx.commit();
 
