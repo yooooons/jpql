f87d71cb94dfc8257c98fabd7a92fce43ab9c821
unknown <yooooonnf@gmail.com>
2022-05-04 23:09:40 +0900

    객체지향 쿼리 언어2 - 중급 문법

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -16,37 +16,45 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("teamA");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
-
-            em.persist(member);
-            //
             Member member1 = new Member();
-            member1.setUsername("team B");
-            member1.setAge(10);
-            member1.setTeam(team);
-            member1.setType(MemberType.ADMIN);
-
-
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
 
             em.flush();
             em.clear();
 
-            String query = "select t from Team t ";
-            List<Team> singleResult = em.createQuery(query, Team.class)
+            String query = "select  t from Team t join fetch t.members";
+            List<Team> resultList = em.createQuery(query, Team.class)
                     .getResultList();
-             singleResult.get(0).getMembers().get(0);
 
+            System.out.println("resultList.size() = " + resultList.size());
+
+            for (Team team : resultList) {
+                System.out.println("team.getName() = " + team.getName() + " |memeber size: " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("->member = " + member);
+                }
+            }
 
             tx.commit();
 
